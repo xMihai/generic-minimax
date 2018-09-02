a16b060c18ac5fef57eab91bb556349a8226778b
Mihai Raducanu <raducanu@gmail.com>
2018-09-02 12:07:04 +0300

    test: updated UTs

--- index.spec.js
+++ index.spec.js
@@ -2,32 +2,102 @@ const { minimax } = require('./index')
 
 describe('generic-minimax', () => {
   test('', () => {
-    console.log(
-      JSON.stringify(
-        minimax({
-          actions: state =>
-            state.reduce(
-              (result, tokens, line) =>
-                tokens > 0
-                  ? [
-                      ...result,
-                      ...new Array(tokens)
-                        .fill('1')
-                        .map((_, i) => ({ line, tokens: i + 1 })),
-                    ]
-                  : result,
-              []
-            ),
-          play: (state, action) =>
-            state.map(
-              (tokens, line) =>
-                line === action.line ? tokens - action.tokens : tokens
-            ),
-          score: state => (state.every(tokens => tokens === 0) ? 1 : 0),
-        })([1, 3, 5, 0]),
-        null,
-        2
-      )
-    )
+    const output = minimax({
+      actions: state =>
+        state.reduce(
+          (result, tokens, line) =>
+            tokens > 0
+              ? [
+                  ...result,
+                  ...new Array(tokens)
+                    .fill('1')
+                    .map((_, i) => ({ line, tokens: i + 1 })),
+                ]
+              : result,
+          []
+        ),
+      play: (state, action) =>
+        state.map(
+          (tokens, line) =>
+            line === action.line ? tokens - action.tokens : tokens
+        ),
+      score: state => (state.every(tokens => tokens === 0) ? 1 : 0),
+    })([1, 3, 5, 0])
+
+    // console.log(JSON.stringify(output, null, 2))
+
+    const expected = {
+      bestAction: {
+        line: 2,
+        tokens: 3,
+      },
+      evaluations: [
+        {
+          value: -1,
+          action: {
+            line: 0,
+            tokens: 1,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 1,
+            tokens: 1,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 1,
+            tokens: 2,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 1,
+            tokens: 3,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 2,
+            tokens: 1,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 2,
+            tokens: 2,
+          },
+        },
+        {
+          value: 1,
+          action: {
+            line: 2,
+            tokens: 3,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 2,
+            tokens: 4,
+          },
+        },
+        {
+          value: -1,
+          action: {
+            line: 2,
+            tokens: 5,
+          },
+        },
+      ],
+    }
+
+    expect(output).toEqual(expected)
   })
 })
