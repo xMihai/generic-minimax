0b9929754375401a8ad0489c4a4613d5e9788c3b
Mihai Raducanu <raducanu@gmail.com>
2018-09-02 12:41:04 +0300

    chore: added comments

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
+// Reducer arguments for choosing best evaluation for maximizing player
 const maximizeArgs = [
   (result, item) => (item.value > result.value ? item : result),
   { value: Number.NEGATIVE_INFINITY },
 ]
 
+// Reducer arguments for choosing best evaluation for minimizing player
 const minimizeArgs = [
   (result, item) => (item.value < result.value ? item : result),
   { value: Number.POSITIVE_INFINITY },
 ]
 
 const minimax = config => initialState => {
+  // Get possible actions for this game state and get their values
   const evaluateActions = (state = initialState, depth = 1) =>
     config.actions(state).map(action => ({
-      value: value(config.nextState(state, action), depth + 1),
       action,
+      value: value(config.nextState(state, action), depth + 1),
     }))
 
-  const value = (nextState = initialState, depth = 1) => {
-    const evaluations = evaluateActions(nextState, depth)
+  // Calculate the value of a game state
+  const value = (state = initialState, depth = 1) => {
+    // Get possible actions and their values
+    const evaluations = evaluateActions(state, depth)
+
     return evaluations.length === 0
-      ? config.score(nextState, Boolean(depth % 2))
-      : evaluations.reduce(...(depth % 2 ? maximizeArgs : minimizeArgs)).value
+      ? // If there are no actions/evaluations, this is a end-game state. Get score.
+        config.score(state, Boolean(depth % 2))
+      : // otherwise get best action value
+        evaluations.reduce(...(depth % 2 ? maximizeArgs : minimizeArgs)).value
   }
 
+  // Best action for maximizing player
   const bestAction = evaluations => {
+    // Aggregate a list of evaluations having the greatest value
     const bestEvaluations = evaluations.reduce(
       (best, evaluation) => {
+        // This evaluation is better than the best, reset list
         if (evaluation.value > best[0].value) return [evaluation]
+
+        // This evaluation is the same as the best, add to list
         if (evaluation.value === best[0].value) return [...best, evaluation]
+
+        // This evaluation is worse than the best, return existing list
         return best
       },
       [
@@ -35,7 +50,11 @@ const minimax = config => initialState => {
         },
       ]
     )
-    return bestEvaluations[Math.floor(Math.random() * bestEvaluations.length)]
+
+    // Out of the best evaluations, choose one at random to create diversity
+    return bestEvaluations.length > 1
+      ? bestEvaluations[Math.floor(Math.random() * bestEvaluations.length)]
+      : bestEvaluations[0]
   }
 
   const evaluations = evaluateActions()
